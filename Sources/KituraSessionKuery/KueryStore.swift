37ed96f2c2597658db4976a93469eb5209292b85
Danny Sung <danny@dannysung.com>
2019-10-09 21:32:25 -0700

    Updated to use ConnectionPool.  And request for connection on use.

--- Sources/KituraSessionKuery/KueryStore.swift
+++ Sources/KituraSessionKuery/KueryStore.swift
@@ -16,55 +16,73 @@ public class KueryStore: Store {
         let data = Column("data", String.self)
     }
 
-    private let db: SwiftKuery.Connection
+    private let pool: SwiftKuery.ConnectionPool
     private var sessions: Sessions
 
-    public init(connection: SwiftKuery.Connection) {
-        db = connection
-        sessions = Sessions()
-        setupDB()
+    public init(pool: SwiftKuery.ConnectionPool) {
+        self.pool = pool
+        self.sessions = Sessions()
+        self.setupDB()
     }
 
     private func setupDB() {
-        sessions.create(connection: db) { result in
-            guard result.success else {
-                print("Failed to create table: \(result)")
+        self.pool.getConnection() { connection, error in
+            guard let connection = connection else {
+                print("Could not create connection to database in setup.  \(error?.localizedDescription)")
                 return
+            }
+            self.sessions.create(connection: connection) { result in
+                guard result.success else {
+                    print("Failed to create table: \(result)")
+                    return
+                }
             }
         }
     }
 
     public func load(sessionId: String, callback: @escaping (Data?, NSError?) -> Void) {
         let query = Select(sessions.data, from: sessions).where(sessions.id == sessionId)
-        db.execute(query: query) { result in
-            guard result.success else {
-                callback(nil, result.asError as NSError?)
+        self.pool.getConnection() { connection, error in
+            guard let connection = connection else {
+                print("Could not create connection to database in all to load(sessionId: \(sessionId).  \(error?.localizedDescription)")
                 return
             }
-
-            result.asRows { rows, error in
-                if let row = rows?.first {
-                    if let base64String = row[self.sessions.data.name] as? String, let decodedData = Data(base64Encoded: base64String) {
-                        callback(decodedData, nil)
-                        return
-                    }
+            connection.execute(query: query) { result in
+                guard result.success else {
+                    callback(nil, result.asError as NSError?)
+                    return
                 }
-                callback(nil, nil)
+    
+                result.asRows { rows, error in
+                    if let row = rows?.first {
+                        if let base64String = row[self.sessions.data.name] as? String, let decodedData = Data(base64Encoded: base64String) {
+                            callback(decodedData, nil)
+                            return
+                        }
+                    }
+                    callback(nil, nil)
+                }
+    
+                // TODO: does this need to wait until completion?
             }
-
-            // TODO: does this need to wait until completion?
         }
     }
 
     public func save(sessionId: String, data: Data, callback: @escaping (NSError?) -> Void) {
         let query = Insert(into: sessions, rows: [[sessionId, data.base64EncodedString()]])
-        db.execute(query: query) { result in
-            guard result.success else {
-                callback(result.asError as NSError?)
+        self.pool.getConnection() { connection, error in
+            guard let connection = connection else {
+                print("Could not create connection to database in all to save(sessionId: \(sessionId).  \(error?.localizedDescription)")
                 return
             }
-
-            callback(nil)
+            connection.execute(query: query) { result in
+                guard result.success else {
+                    callback(result.asError as NSError?)
+                    return
+                }
+    
+                callback(nil)
+            }
         }
     }
 
@@ -74,12 +92,18 @@ public class KueryStore: Store {
 
     public func delete(sessionId: String, callback: @escaping (NSError?) -> Void) {
         let query = Delete(from: sessions, where: sessions.id == sessionId)
-        db.execute(query: query) { result in
-            guard result.success else {
-                callback(result.asError as NSError?)
+        self.pool.getConnection() { connection, error in
+            guard let connection = connection else {
+                print("Could not create connection to database in all to delete(sessionId: \(sessionId).  \(error?.localizedDescription)")
                 return
             }
-            callback(nil)
+            connection.execute(query: query) { result in
+                guard result.success else {
+                    callback(result.asError as NSError?)
+                    return
+                }
+                callback(nil)
+            }
         }
     }
 }
